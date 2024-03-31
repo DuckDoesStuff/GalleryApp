96ef46fafa2afe3d1275a9710e826c2cbb4e7d0c
Phuong Banh <m.phuong219@gmail.com>
2024-04-01 00:54:37 +0700

    01-4-2003

--- app/src/main/java/com/example/gallery/activities/ImageActivity.java
+++ app/src/main/java/com/example/gallery/activities/ImageActivity.java
@@ -2,6 +2,7 @@ package com.example.gallery.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,11 +34,27 @@ public class ImageActivity extends AppCompatActivity {
         ImageButton imageButton = findViewById(R.id.back_button);
         imageButton.setOnClickListener(v -> finish());
 
+
         ViewPager2 viewPager2 = findViewById(R.id.view_pager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(images);
         viewPager2.setAdapter(viewPagerAdapter);
         viewPager2.setOffscreenPageLimit(5);
         viewPager2.setCurrentItem(position, false);
+
+        ImageButton editButton = findViewById(R.id.edit_btn);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Thực hiện chuyển sang EditActivity
+                String currentImagePath = images.get(viewPager2.getCurrentItem());
+
+                // Thực hiện chuyển sang EditActivity và truyền đường dẫn của hình ảnh
+                Intent editIntent = new Intent(ImageActivity.this, EditActivity.class);
+                editIntent.putExtra("imagePath", currentImagePath);
+                startActivity(editIntent);
+            }
+        });
+
     }
 
 }
